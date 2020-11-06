6b1c4c4dfc9ecd14d501932c184db55158cd225c
Faruk Gürlesin <farukgurlesin@gmail.com>
2020-11-06 16:33:39 +0300

    Edit Update Create eklendi

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,37 @@ class ArticlesController < ApplicationController
     @article = Article.all 
   end
 
+  def new
+    @article=Article.new
+  end
+  
+  def create 
+    @article=Article.new(params.require(:article).permit(:tittle, :description))   
+    if @article.save
+      flash[:notice] = "Article was created succesfully"
+      #redirect_to articles_path
+      redirect_to @article
+    else
+      render 'new'
+    end
+    
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update 
+    @article=Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:tittle, :description))
+      flash[:notice]="Article was updated succesfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
+
+
   
 end
