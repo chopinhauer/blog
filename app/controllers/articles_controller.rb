8ee09b80187dfc33e87bdc1c2971028bced22336
Faruk Gürlesin <farukgurlesin@gmail.com>
2020-11-10 11:26:51 +0300

    DRY edit-new (form) , private article  methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article , only: [ :show , :edit , :update , :destroy]
+
   def show
     # byebug
-    @article = Article.find(params[:id])
   end
 
   def index 
@@ -13,7 +15,7 @@ class ArticlesController < ApplicationController
   end
   
   def create 
-    @article=Article.new(params.require(:article).permit(:tittle, :description))   
+    @article = Article.new(article_params)   
     if @article.save
       flash[:notice] = "Article was created succesfully"
       #redirect_to articles_path
@@ -25,12 +27,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def update 
-    @article=Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:tittle, :description))
+    if @article.update(article_params)
       flash[:notice]="Article was updated succesfully"
       redirect_to @article
     else
@@ -39,9 +40,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article=Article.find(params[:id])
     @article.destroy
     redirect_to articles_path 
   end
+
+  private
+
+  def set_article
+  @article=Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:tittle, :description)
+  end
+
+  public
 
 end
